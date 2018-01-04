1169a54cc6c5b27149dc136625279f3bf9a9aff5
Gustavo <consugus@gmail.com>
2018-01-03 22:57:26 -0300

    Setting up the script page

--- Cracking the Coding Interview/12 - HashTable-Ice Cream Parlor/script1.js
+++ Cracking the Coding Interview/12 - HashTable-Ice Cream Parlor/script1.js
@@ -1,14 +1,15 @@
 //Input
-
+var t = 2;
+var arr = [4, 5, [1, 4, 5, 3, 2], 4, 4, [2, 2, 4, 3]];
 
 
 //Solution
-function eval(n, str){
+function eval(t, arr){
 
-    //return "Hola";
+    return "Hola";
 };
 
 
 
-retorno = eval(n, str);
+retorno = eval(t, arr);
 
