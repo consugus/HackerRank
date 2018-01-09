d0faec8ac3c06cda56473411ca08fbaf6f53355c
Gustavo <consugus@gmail.com>
2018-01-09 01:17:15 -0300

    Setting up the script page

--- Cracking the Coding Interview/11 - Merge Sort - Counting Invertions/script1.js
+++ Cracking the Coding Interview/11 - Merge Sort - Counting Invertions/script1.js
@@ -1,15 +1,19 @@
 //Input
-
+var d = 2;
+// var n1 = 5;
+// var arr1 = [1, 1, 1, 2, 2];
+var n2 = 5;
+var arr2 = [2, 1, 3, 1, 2];
 
 
 //Solution
-function eval(money, n, arr){
-
+function eval(n, arr){
+    console.log(arr)
 
     return "Hola";
 };
 
 
 
-retorno = eval(money, n, arr);
+retorno = eval(n2, arr2);
 
