1a6c6c83522413db000a2ec6254f23ab5360f146
Gustavo <consugus@gmail.com>
2018-01-24 15:39:56 -0300

    16 - Solved!

--- Cracking the Coding Interview/16 - Recursion-Fibonacci Numbers/script1.js
+++ Cracking the Coding Interview/16 - Recursion-Fibonacci Numbers/script1.js
@@ -1,37 +1,19 @@
 //Input
-// var p = 3;
-// var arr = [12, 5, 7];
-
-var p = 30;
-var arr = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121, 144, 169, 196, 225, 256, 289, 324, 361, 400, 441, 484, 529, 576, 625, 676, 729, 784, 841, 907];
-
-// var p = 1;
-// var arr = [17];//907
+var n = 4;
 
 //Solution
-function solve(p, arr){
-    var result;
-    for(var i = 0 ; i < p ; i++){
-        if(seeIfPrime(arr[i])){
-            result = "" + arr[i] + ": " + "Prime";
-        } else{
-            result = "" + arr[i]  + ": " + "Not prime";
-        };
-        console.log(result);
+function fibonacci(n){
+    if (n > 1){
+        return ( fibonacci(n-1) + fibonacci(n-2) ); //función revursiva
     };
-    return "Hola";
-}
-
-function seeIfPrime(num){
-    if (num == 1 || num == 4){
-        return false;
+    if(n == 0){ //caso base
+        //console.log("entró en el cero");
+        return 0;
     };
-    for(var i = 2 ; i < Math.ceil(num/2) ; i++){
-        if( (num % i) == 0 ){
-            return false;
-        };
+    if(n == 1){ //caso base
+        //console.log("entró en el cero");
+        return 1;
     };
-    return true;
 };
 
-var retorno = solve(p, arr);
+var retorno = fibonacci(n);
