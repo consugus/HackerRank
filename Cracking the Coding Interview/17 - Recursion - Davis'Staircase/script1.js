15fd6016d6806164df23348afa4bdff6661aa924
Gustavo <consugus@gmail.com>
2018-01-26 09:42:16 -0300

    17 - Solved!

--- Cracking the Coding Interview/17 - Recursion - Davis'Staircase/script1.js
+++ Cracking the Coding Interview/17 - Recursion - Davis'Staircase/script1.js
@@ -1,9 +1,24 @@
 //Input
+//  * 1 <= s <= 5
+//  * 1 <= n <= 36
+
 var s = 4;
+//var n = 1; //1
+var n = 3; //4
+var n = 7; //44
 
 //Solution
-function solve(s){
+function solve(n){
+   console.log(fibonacci(n-1));
    return "hola";
 };
 
-var retorno = solve(s);
+function fibonacci(n){
+    var fib = [1, 2, 4];
+    for(var i = 3 ; i <= n ; i++){
+        fib[i%3] = fib[0] + fib[1] + fib[2];
+    };
+    return fib[n%3];
+};
+
+var retorno = solve(n);
