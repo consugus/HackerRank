5926644967fd843eeee132b0e8a3b257efdfa7d5
Gustavo <consugus@gmail.com>
2018-02-17 10:50:48 -0300

    RR - 01 Rush Exam Solved!

--- RookieRank4/03 - RR Exam Rush/script1.js
+++ RookieRank4/03 - RR Exam Rush/script1.js
@@ -1,40 +1,37 @@
 //Input
-var n = 2; // number of chapters
-var t = 2; //time left until exam
+
+var n = 2; // 1 <= n <= 10.000  [number of chapters]
+var t = 2; // 1 <= t <= 10^9    [hours until exam]
 var tm = [1, 2];
 
 var n = 4;
-var t = 5;
-var tm = [4, 3, 1, 1];
+var t = 10;
+var tm = [3, 5, 1, 1];
 
 retorno = eval(n, tm, t);
 
 //Solution
 function eval(n, tm, t){
-    var answer = 0;
-    //console.log("n: " + n);
-    //for(var i = 0 ; i < n ; i++){
-        answer = examRush2(tm, t);
-    //};
-    console.log("answer: " + answer);
-    return "Hola";
+    console.log(examRush(n, tm, t));
+    return "Holita";
 };
 
 
-function examRush(tm, t){
+function examRush(n, tm, t){
     var maxNoCh = 0;
     var totalTime = 0;
-    tm.sort();
-    console.log("tm: " + tm + "\n\n");
-    for(var i = 0 ; i< tm.length ; i++){
-        if( (totalTime + tm[i])  <= t){
+    tm.sort(function(a,b){ return a-b});
+
+    //In case all values exceed t, no need see inside the array
+    if(tm[0] > t){   return 0;   };
+
+    for(var i = 0 ; i < n ; i++){
+        if( (totalTime + tm[i]) <= t ){
             totalTime += tm[i];
             maxNoCh++;
         };
-
     };
 
-    console.log("maxNoCh: " + maxNoCh);
     return maxNoCh;
 };
 
--- RookieRank4/04 -RR Server Room Safety/script1.js
+++ RookieRank4/04 -RR Server Room Safety/script1.js
@@ -1,11 +1,38 @@
 //Input
+var n = 5; //                      0 < n <= 100, [number of racks]
+var position = [1, 2, 3, 4, 5]; // 0 < x[i] <= 1000, [position of each rack]
+var height   = [1, 1, 1, 1, 1]; //    0 < h[i] <= 1000, [height of each rack]
+                                           // BOTH
+
+var n = 5;
+var position = [1, 2, 3, 4, 8];
+var height   = [1, 1, 1, 1, 1];            // NONE
+
+var n = 2;
+var position = [1, 4];
+var height   = [3, 2];                     // LEFT
 
 
-retorno = eval();
+
+retorno = checkAll(n, position, height);
 
 //Solution
-function eval(){
+function checkAll(n, position, height){
+    var left = false;
+    var rigth = false;
+    for(var i = 1 ; i <= n ; i++){
+        if(true){
 
+        };
+    };
+
+
+
+
+    
+
+    console.log("holita");
+    return "Hola";
 };
 
 
