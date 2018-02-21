c0ceee14cefe177dc30b510e09e8ed1280711a5d
Gustavo <consugus@gmail.com>
2018-02-21 18:02:37 -0300

    Strings - 02-camelCase-Slved!

--- 03.Strings/02 - CamelCase - Easy15/script1.js
+++ 03.Strings/02 - CamelCase - Easy15/script1.js
@@ -1,11 +1,22 @@
 //Input
+var s = "saveChangesInTheEditor"; // 5
 
-
-retorno = eval();
+retorno = camelCase(s);
 
 //Solution
-function eval(){
+function camelCase(s){
+    var upperCaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var answer = 0;
 
+    for (var i = 0 ; i < s.length ; i++){
+        for(var j = 0 ; j < upperCaseAlphabet.length ; j++){
+            if(s[i] == upperCaseAlphabet[j]){
+                answer++;
+            };
+        };
+    };
+
+    return answer+1;
 };
 
 
