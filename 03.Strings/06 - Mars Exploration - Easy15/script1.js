d40eddf9a0cb463cb29fd7d953f2ccbed98284d7
Gustavo <consugus@gmail.com>
2018-02-23 21:34:48 -0300

    Strings - 06 - MarsExploration - Solved!

--- 03.Strings/06 - Mars Exploration - Easy15/script1.js
+++ 03.Strings/06 - Mars Exploration - Easy15/script1.js
@@ -6,12 +6,19 @@ retorno = marsExploration(s);
 
 //Solution
 function marsExploration(s){
-    var answer = "Hola";
-
-
-
-
-    return answer;
+    var numberOfChanges = 0;
+    for (var i = 0 ; i < s.length ; i+=3){
+        if(s[i] != "S"){
+            numberOfChanges ++;
+        };
+        if(s[i+1] != "O"){
+            numberOfChanges++;
+        };
+        if(s[i+2] != "S"){
+            numberOfChanges++;
+        };
+    };
+    return numberOfChanges;
 };
 
 
