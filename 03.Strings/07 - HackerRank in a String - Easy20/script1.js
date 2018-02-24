47e02346179eb4b66e21a945c076b9d38a880be4
Gustavo <consugus@gmail.com>
2018-02-23 21:56:38 -0300

    Strings 07 - Hackerrank in a String - Declaration

--- 03.Strings/07 - HackerRank in a String - Easy20/script1.js
+++ 03.Strings/07 - HackerRank in a String - Easy20/script1.js
@@ -1,11 +1,28 @@
 //Input
+var s = "hereiamstackerrank"; // YES
+var s = "hackerworld"; //NO
+//var s = "hola";
 
-
-retorno = eval();
+retorno = hackerrankInString(s);
 
 //Solution
-function eval(){
+function hackerrankInString(s){
+    var answer = "NO";
+    var stringToCheck = "hackerrank";
 
+    if( !(s.length < 9) ){
+        //for(var i = 0 ; i < stringToCheck.length ; i++){
+            var i = 0;
+            for(var j = 0 ; j < s.length ; j++){
+                if(s[j] == stringToCheck[i]){
+                    
+                };
+            };
+        //};
+    };
+
+
+    return answer;
 };
 
 
