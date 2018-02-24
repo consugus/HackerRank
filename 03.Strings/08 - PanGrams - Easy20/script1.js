3e55b202d83cf904f169eb1381d12ea592e24497
Gustavo <consugus@gmail.com>
2018-02-24 17:20:06 -0300

    Strings - 08 - Pangrams - Declaration

--- 03.Strings/08 - PanGrams - Easy20/script1.js
+++ 03.Strings/08 - PanGrams - Easy20/script1.js
@@ -1,4 +1,6 @@
 //Input
+var s = "We promptly judged antique ivory buckles for the next prize" // pangram
+//var s = "We promptly judged antique ivory buckles for the prize"; // not pangram
 
 
 retorno = eval();
