710cde21a2d36d29aa4b85fec032877d0704cf1a
Gustavo <consugus@gmail.com>
2018-02-24 18:29:53 -0300

    Strings - 09 - Weighted Uniforms Strings - Declaration

--- 03.Strings/09 - Weighted Uniforms Strings - Easy20/script1.js
+++ 03.Strings/09 - Weighted Uniforms Strings - Easy20/script1.js
@@ -1,11 +1,17 @@
 //Input
+var s = "abccddde"; //Original String
+var n = 6; //Number of queries
+var x = [1, 3, 13, 5, 9, 10]; //each string to test
 
-
-retorno = eval();
+retorno = eval(s, n, x);
 
 //Solution
-function eval(){
+function eval(s, n, x){
+    var answer = "Yes";
 
+
+
+    return answer;
 };
 
 
