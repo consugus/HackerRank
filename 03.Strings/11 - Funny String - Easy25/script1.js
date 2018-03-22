68fbedef2dcd287e1f81ffec747cc7b887316ae5
Gustavo <consugus@gmail.com>
2018-03-22 20:59:02 -0300

    11 - Funny Strings - Declaration

--- 03.Strings/11 - Funny String - Easy25/script1.js
+++ 03.Strings/11 - Funny String - Easy25/script1.js
@@ -1,11 +1,29 @@
 //Input
+var t = 2;
+var s = ["acxz", "bcxz"] // Funny, Not Funny
 
+var t = 1;
+var s = ["acxz"];
 
-retorno = eval();
+retorno = eval(t, s);
 
 //Solution
-function eval(){
+function eval(t, s){
+    for (var i = 0 ; i < t ;  i++){
+        
+        console.log(funnyString(s[i]));
+    };
+    return "Hola";
+};
 
+function funnyString(s){
+    var r = s.reverse();
+    console.log(s, r);
+    var answer = "Funny";
+
+
+
+    return answer;
 };
 
 
