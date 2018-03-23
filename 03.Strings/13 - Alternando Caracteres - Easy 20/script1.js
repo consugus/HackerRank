f4a6ef0d1d7b48bcaac1911d67b0a0484f3aa82a
Gustavo <consugus@gmail.com>
2018-03-23 20:32:08 -0300

    Strings -13 - Alternando Caracteres - Declaration

--- 03.Strings/13 - Alternando Caracteres - Easy 20/script1.js
+++ 03.Strings/13 - Alternando Caracteres - Easy 20/script1.js
@@ -1,11 +1,23 @@
 //Input
+var t = 5
+var arr = [];
 
-
-retorno = eval();
+retorno = eval(t, arr);
 
 //Solution
-function eval(){
+function eval(t, arr){
+    for(var i = 0 ; i < t ; i++){
+        console.log(alternatingCharacters(arr[i]));
+    };
+    return "Hola";
+};
 
+function alternatingCharacters(s){
+    var charactersDeleted = 0;
+
+
+
+    return charactersDeleted;
 };
 
 
