88e322367a2df0381e6b8eff85b7d07a18c55e40
Gustavo <consugus@gmail.com>
2018-03-23 12:42:52 -0300

    12 - Gem Stones - Declaration

--- 03.Strings/12 - Gem Stones -Easy20/script1.js
+++ 03.Strings/12 - Gem Stones -Easy20/script1.js
@@ -1,10 +1,18 @@
 //Input
+var n = 3;                                 // 1 ≤ N ≤ 100
+var arr = ["abcdde", "baccd", "eeabg"]; // 2
 
-
-retorno = eval();
+retorno = eval(n, arr);
 
 //Solution
-function eval(){
+function eval(n, arr){
+    var answer = 0;
+
+
+    return answer;
+};
+
+function gemStones(arr){
 
 };
 
