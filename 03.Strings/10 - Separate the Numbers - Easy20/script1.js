9145ccad856c7baad609e4933371c400b217fd24
Gustavo <consugus@gmail.com>
2018-03-22 22:55:18 -0300

    10 - Separate the Numbers --> solved Test Case 16. Still with problems with Test Case 20

--- 03.Strings/10 - Separate the Numbers - Easy20/script1.js
+++ 03.Strings/10 - Separate the Numbers - Easy20/script1.js
@@ -2,15 +2,24 @@
 var q = 7;                                              // 1 <= q <= 10
 var s = ["1234", "91011", "99100", "101103", "010203", "13", "1"];    // 1 <= |s| <= 32
 
-var q = 1;
-var s = ["96589218797811259658921879781126"]; // YES 9658921879781125
+// var q = 1;
+// var s = ["4294967295429496729642949672"]; // YES 4294967295
 
 // var q = 10;
 // var s = ["51073756645096775107375664509678", "51073756645096775107375664509668", "96589218797811259658921879781126", "96589218797811259658921879781026", "960567818611458960567818611459", "960567818611458960567818610459", "65651460403999656565146040399966", "65651460403999656565146040389966", "84137510565804148413751056580415", "84137510565804148413751056480415"];
 // falla en el 3 número   var s = ["96589218797811259658921879781126"]; // YES 9658921879781125
 
-var q = 10;
-var s = ["429496729542949672964294967297", "429496729542949672964294967296", "429496729542949672964294967287", "429496729542949672964294967197", "42949672954294967296429496729", "4294967295429496729642949672", "429496729500000000000000000001", "42949672950123456789", "4294967295000010020030000456789", "4294967295000102003004005"];
+// var q = 10;
+// var s = ["429496729542949672964294967297", 
+// "429496729542949672964294967296", 
+// "429496729542949672964294967287", 
+// "429496729542949672964294967197", 
+// "4294967295 4294967296 429496729", 
+// "4294967295429496729642949672", 
+// "429496729500000000000000000001", 
+// "42949672950123456789", 
+// "4294967295000010020030000456789", 
+// "4294967295000102003004005"];
 
 retorno = eval(q, s);
 
@@ -41,8 +50,11 @@ function separateNumbers(s){
             //console.log("first: " + first + "    second: " + second + "    size: " + size);
 
             answer = areBeautiful(first, second);
-            //console.log("areBeautiful: " + areBeautiful(first, second));
+
+
+            //console.log("answer: " + answer + "    first: " + first + "    second: " + second);
             if(!answer){
+                //console.log("answer: " + answer);
                 break;
             };
             first = second;
@@ -52,10 +64,11 @@ function separateNumbers(s){
                 break;
             };
         };
-        //console.log("    answer    " + answer);
+        //console.log("    answer    " + answer + "    final == 'NO': " + (final == "NO"));
+
         if(answer && final == "NO"){
-            final = "YES " + firstToShow;
-        };
+            final = " YES " + firstToShow;
+        }
     };
     return  final;
 };
@@ -63,20 +76,38 @@ function separateNumbers(s){
 
 
 function areBeautiful(first, second){
-    var rule1 = checkRule1(first, second);
+    var rule1 = checkRule1Bis(first, second);
     var rule2 = checkRule2(first, second);
-    //console.log("rule1: " + rule1 + "    rule2: " + rule2);
     var beautiful = rule1 && rule2 ;
     return beautiful;
 };
 
 function checkRule1(first, second){
-    // console.log("checkRule1: " + ( parseInt(first) + 1 == parseInt(second) ));
-    // console.log(first);
-    // console.log(second);
-    // console.log(parseInt(first));
-    // console.log(parseInt(second));
     return (parseInt(first) + 1 == parseInt(second));
+};
+
+function checkRule1Bis(first, second){
+    var f = 0;
+
+    if(areAllNines(first)){
+        f = "1";
+        for(var i = 0 ; i < first.length ; i++){
+            f+= "0";
+        };
+    } else{
+        var index = 0;
+        for(var i = first.length -1 ; i >= 0 ; i--){
+            if(first[i] != "9"){
+                index = i;
+                break;
+            };
+        };
+        var f = first.slice(0, (index)) + (parseInt(first[index]) + 1);
+        for(var i = 0 ; i < (first.length - index - 1) ; i++){
+            f += "0";
+        };
+    };
+    return (f == second);
 };
 
 function checkRule2(first, second){
