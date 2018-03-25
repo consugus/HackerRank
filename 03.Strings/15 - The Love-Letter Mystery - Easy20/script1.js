d4af9aef6dc97d523491a087f93fb11a074550e7
Gustavo <consugus@gmail.com>
2018-03-25 12:28:33 -0300

    Strings - 15 The Love-Letter Mystery - Declaration

--- 03.Strings/15 - The Love-Letter Mystery - Easy20/script1.js
+++ 03.Strings/15 - The Love-Letter Mystery - Easy20/script1.js
@@ -1,11 +1,24 @@
 //Input
+var t = 3;  // 1 <= t <= 10
+var arr = ["abc", "abcba", "abcd"];
 
+var t = 1;
+var arr = ["abcba"];
 
-retorno = eval();
+retorno = eval(t, arr);
 
 //Solution
-function eval(){
+function eval(t, arr){
+    for(var i = 0 ; i < t ; i++){
+        console.log(theLoveLetterMystery(arr[i]));
+    };
+};
 
+function theLoveLetterMystery(str){
+    var operationNumber = 0;
+
+
+    return operationNumber;
 };
 
 
