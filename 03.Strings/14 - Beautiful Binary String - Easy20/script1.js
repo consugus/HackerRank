4d5d9dce3b9c6f0a93b3b0f097baff1a30988fbf
Gustavo <consugus@gmail.com>
2018-03-25 12:12:04 -0300

    Strings - 14 - Beautiful Binary String - Solved!

--- 03.Strings/14 - Beautiful Binary String - Easy20/script1.js
+++ 03.Strings/14 - Beautiful Binary String - Easy20/script1.js
@@ -1,12 +1,12 @@
 //Input
-var n = 7;
+var n = 7;         //1 <= n <= 100
 var b = "0101010"; // 2
 
-// var n = 5;
-// var b = "01100" // 0
+var n = 5;
+var b = "01100" // 0
 
-// var n = 10;
-// var b = "0100101010"; // 3
+var n = 10;
+var b = "0100101010"; // 3
 
 retorno = eval(b);
 
@@ -17,8 +17,12 @@ function eval(b){
 
 function beautirulBinaryString(b){
     var steps = 0;
-
-
+    for(var i = 0 ; i < (b.length -2) ; i++){
+        if(b[i] == "0" && b[i+1] == "1" && b[i+2] == "0"){
+            steps++;
+            i+=2;
+        };
+    };
     return steps;
 };
 
