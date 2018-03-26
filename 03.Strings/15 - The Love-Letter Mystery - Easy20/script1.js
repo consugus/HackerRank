7794efaab2b2920a67a7a0dc5a970db544d82d02
Gustavo <consugus@gmail.com>
2018-03-26 09:47:22 -0300

    Strings - 15 - The Love-Letter Mystery - Solved!

--- 03.Strings/15 - The Love-Letter Mystery - Easy20/script1.js
+++ 03.Strings/15 - The Love-Letter Mystery - Easy20/script1.js
@@ -1,9 +1,9 @@
 //Input
-var t = 3;  // 1 <= t <= 10
-var arr = ["abc", "abcba", "abcd"];
+var t = 4;  // 1 <= t <= 10
+var arr = ["abc", "abcba", "abcd", "cba"];
 
-var t = 1;
-var arr = ["abcba"];
+// var t = 1;
+// var arr = ["abcd"];
 
 retorno = eval(t, arr);
 
@@ -16,8 +16,11 @@ function eval(t, arr){
 
 function theLoveLetterMystery(str){
     var operationNumber = 0;
-
-
+    for(var i = 0 ; i < str.length/2 ; i++){
+        //console.log("str.chartAt[" + i + "]: " + str.charAt(i));
+        operationNumber += Math.abs( str.charCodeAt(i) - str.charCodeAt(str.length - i - 1) );
+        //console.log("diff: " + diff);
+    };
     return operationNumber;
 };
 
