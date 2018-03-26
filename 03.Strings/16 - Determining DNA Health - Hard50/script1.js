b0c6b407ecca9ed0b2a527705598df26e673e66b
Gustavo <consugus@gmail.com>
2018-03-26 10:16:29 -0300

    Strings - 16 - Determining DNA Health - Declaration

--- 03.Strings/16 - Determining DNA Health - Hard50/script1.js
+++ 03.Strings/16 - Determining DNA Health - Hard50/script1.js
@@ -1,5 +1,9 @@
 //Input
-
+var n = 6;
+var genes = ["a", "b", "c", "aa", "d", "b"];
+var health = [1, 2, 3, 4, 5, 6];
+var s = 3;
+var arr = [ [1, 5, "caaab"], [0, 4, "xyz"], [2, 4, "bcdybc"] ];
 
 retorno = eval();
 
