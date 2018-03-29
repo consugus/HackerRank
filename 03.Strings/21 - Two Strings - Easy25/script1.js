32b3b5f49ffec48bc08bd032f98ef20d80d4d985
Gustavo <consugus@gmail.com>
2018-03-28 21:55:50 -0300

    Strings - 21 - Two Strings - Declaration

--- 03.Strings/21 - Two Strings - Easy25/script1.js
+++ 03.Strings/21 - Two Strings - Easy25/script1.js
@@ -1,11 +1,24 @@
 //Input
+var p = 2;
+var arr = [["hello", "world"], ["hi", "world"]];
 
+var p = 1;
+var arr = [["hello", "world"]];
 
-retorno = eval();
+retorno = eval(p, arr);
 
 //Solution
-function eval(){
+function eval(p, arr){
+    for(var i = 0 ; i < arr.length ; i++){
+        console.log(twoStrings(arr[i][0], arr[i][1]));
+    };
+};
 
+function twoStrings(s1, s2){
+    var haveSubstringsInCommon = false;
+    console.log("s1: " + s1 + "    s2: " + s2);
+
+    return haveSubstringsInCommon;
 };
 
 
