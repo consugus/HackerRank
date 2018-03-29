062709bc8f28b39786f7c96b7e2d16bdb54ba465
Gustavo <consugus@gmail.com>
2018-03-28 22:30:00 -0300

    Strings - 22 - String Construction - Declaration

--- 03.Strings/22 - String Construction - Easy25/script1.js
+++ 03.Strings/22 - String Construction - Easy25/script1.js
@@ -1,11 +1,26 @@
 //Input
+var n = 2;
+var arr = ["abcd", "abab"]; // 4, 2
 
+// var n = 1;
+// var arr = ["abab"];
 
-retorno = eval();
+retorno = eval(n, arr);
 
 //Solution
-function eval(){
+function eval(n, arr){
+    for(var i = 0 ; i < arr.length ; i++){
+        console.log(stringConstruction(arr[i]));
+    };
+    return "Hola";
+};
 
+function stringConstruction(s){
+    var totalCost = 0;
+    var len = s.length;
+    //console.log("s: " + s + "(" + len + ")");
+
+    return totalCost;
 };
 
 
