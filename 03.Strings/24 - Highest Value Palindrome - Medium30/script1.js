dbcabc21c63bc8e9b6dd9663a07e76382ee1794d
Gustavo <consugus@gmail.com>
2018-04-05 13:14:58 -0300

    Strings - 24 - Highest Value Palindrome - Declaration

--- 03.Strings/24 - Highest Value Palindrome - Medium30/script1.js
+++ 03.Strings/24 - Highest Value Palindrome - Medium30/script1.js
@@ -1,11 +1,21 @@
 //Input
+var s = "3943"; var n = 4; var k = 1;
 
+// var s = "092282"; var n = 6; var k = 3;
 
-retorno = eval();
+// var s = "0011"; var n = 4; var k = 1;
+
+retorno = eval(s, n, k);
 
 //Solution
-function eval(){
+function eval(s, n, k){
+    console.log(highestValuePalindrome(s, n, k));
+    return "Hola";
+};
 
+function highestValuePalindrome(s, n, k){
+
+    return 1;
 };
 
 
