a53984a9694080b854693fd80d06d40fb839b8b7
Gustavo <consugus@gmail.com>
2018-04-05 21:18:34 -0300

    Strings 25 - Maximum Palindromes - Declaration

--- 03.Strings/25 - Maximum Palindromes - Medium40/script1.js
+++ 03.Strings/25 - Maximum Palindromes - Medium40/script1.js
@@ -1,10 +1,14 @@
 //Input
+var s = "week"; var q = 2 ; var lr = [ [1, 4], [2, 3]]; // 2 1
+
+//var s = "abab"; var q = 1; var lr = [[1, 4]];
 
 
-retorno = eval();
+
+retorno = eval(s, q, lr);
 
 //Solution
-function eval(){
+function eval(s, q, lr){
 
 };
 
