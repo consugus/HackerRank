dd34cbcf4ebaf57488eac3885aba26c237fe2694
Gustavo <consugus@gmail.com>
2018-04-16 19:35:45 -0300

    Revert "Re-arrange of "Week of Code36" and renamed to "WeekOfCode". Subfolders were made."
    This reverts commit 44eaae22890e9887339a8d39fee7c2bd08a491b3.

---
+++ WeekOfCode36/05 - Cut a Strip/script1.js
@@ -0,0 +1,32 @@
+////Input
+////Sample 0
+var n = 2;
+var m = 4;
+var k = 2;
+var A = [[1, -3, 4, -5], [2, 1, -7, -2]]; // 5
+
+////Sample 1
+var n = 3;
+var m = 4;
+var k =  3;
+var A = [[-10, 1, 1, 1], [-10, 1, -1, 1], [-10, 1, 1, 1]]; // 8
+
+////Sample 1
+var n = 3;
+var m = 2;
+var k = 1;
+var A = [[1, 2], [3, 4], [5, 6]]; // 20
+
+
+retorno = cutAStrip(A);
+
+////Solution
+function cutAStrip(A){
+
+    return "Hola";
+};
+
+
+
+
+
---
+++ WeekOfCode36/06 - Expert Computation/script1.js
@@ -0,0 +1,25 @@
+////Input
+//Be careful!!!!, you have to read/parse the input///
+
+        ////Sample0
+var n = 5; // length of the arrays
+var arrX = [3, 4, 14, 5, 6];
+var arrY = [4, 9, 12, 8, 9];
+var arrZ = [0, 0, 9, 12, 13];   // 17
+
+        ////Sample1
+// var n = 3; // length of the arrays
+// var arrX = [2, 6, 4];
+// var arrY = [1, 9, 11];
+// var arrZ = [0, 0, 14];   // 17
+
+//Solution
+function expertComputation(){
+
+    return "Hola";
+};
+
+
+
+retorno = expertComputation();
+
