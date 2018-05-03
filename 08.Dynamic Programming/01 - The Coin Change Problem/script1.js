40e67b115064fa69ddc258031b628dcc336c77d9
Gustavo <consugus@gmail.com>
2018-05-03 18:28:20 -0300

    Add files via upload
    New directory structure

---
+++ 08.Dynamic Programming/01 - The Coin Change Problem/script1.js
@@ -0,0 +1,14 @@
+//Input
+var firstLine = "4, 3"; var secondLine = "1, 2, 3";     // 4
+var firstLine = "10 4"; var secondLine = "2, 5, 3, 6";  // 5
+
+retorno = eval(firstLine, secondLine);
+
+//Solution
+function eval(firstLine, secondLine){
+    
+
+};
+
+
+
