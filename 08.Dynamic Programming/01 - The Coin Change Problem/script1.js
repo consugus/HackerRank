52b750b3ca6732b52ee318e68b816bd65cdaab72
Gustavo <consugus@gmail.com>
2018-05-08 02:01:57 -0300

    First attemp

--- 08.Dynamic Programming/01 - The Coin Change Problem/script1.js
+++ 08.Dynamic Programming/01 - The Coin Change Problem/script1.js
@@ -1,14 +1,29 @@
 //Input
-var firstLine = "4, 3"; var secondLine = "1, 2, 3";     // 4
-var firstLine = "10 4"; var secondLine = "2, 5, 3, 6";  // 5
+//#region legend
+// n --> number of units                    1 <= n <= 250
+// m --> number of coins type               1 <= m <= 50
+// c --> values of each coin available
+//       in infinite amounts                1 <= ci <= 50
+//#endregion
+
+var firstLine = "4 3"; var secondLine = "1 2 3";     // 4
+// var firstLine = "10 4"; var secondLine = "2 5 3 6";  // 5
 
 retorno = eval(firstLine, secondLine);
 
 //Solution
 function eval(firstLine, secondLine){
-    
-
+    var nm = firstLine.split(" ").map(x => parseInt(x));
+    var n = nm[0]; var m = nm[1];
+    var coins = secondLine.split(" ").map(x => parseInt(x));
+    console.log(getWays(n,coins));
+    return "Hola";
 };
 
+function getWays(n, c){
 
+
+
+    return 2;
+};
 
