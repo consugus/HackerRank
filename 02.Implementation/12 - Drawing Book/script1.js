5ba9cebd48ecd794c46d60b8510bda60f48ec955
Gustavo <consugus@gmail.com>
2018-05-16 18:50:27 -0300

    Implementation - 12 - Drawing Book - Declaration

---
+++ 02.Implementation/12 - Drawing Book/script1.js
@@ -0,0 +1,20 @@
+//Input
+var n = 6; var p = 2;
+//var n = 5; var p = 4;
+
+retorno = eval(n, p);
+
+//Solution
+function eval(n, p){
+
+    console.log(pageCount(n, p));
+    return "Hola";
+};
+
+
+function pageCount(n, p){
+
+
+    
+    return 10;
+};
