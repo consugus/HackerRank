be99619513e591a5c759cb0866556251f66cc154
Gustavo <consugus@gmail.com>
2018-05-17 10:26:54 -0300

    Implementation - 13-Counting Valleys - Declaration

---
+++ 02.Implementation/13 - Counting Valleys/script1.js
@@ -0,0 +1,18 @@
+//Input
+var n = 8; var s = "UDDDUDUU"; // 1
+
+retorno = eval(n, s);
+
+//Solution
+function eval(n, s){
+
+    console.log(countingValleys(n, s));
+    return "Hola";
+};
+
+
+
+function countingValleys(n, s){
+
+    return 15;
+};
