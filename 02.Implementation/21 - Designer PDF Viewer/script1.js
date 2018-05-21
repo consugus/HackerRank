98b6bde8a0871592c3f9fda3d68a4cf0875ef0e6
Gustavo <consugus@gmail.com>
2018-05-21 17:54:17 -0300

    Implementation - 21-Designer PDF Viewer - Declaration

--- 02.Implementation/21 - Designer PDF Viewer/script1.js
+++ 02.Implementation/21 - Designer PDF Viewer/script1.js
@@ -1,12 +1,21 @@
 //Input
+var firstLine = "1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5"; var secondLine = "abc"; // 9
+var firstLine = "1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 7"; var secondLine = "zaba"; // 28
 
-
-retorno = eval();
+retorno = eval(firstLine, secondLine);
 
 //Solution
-function eval(){
+function eval(firstLine, secondLine){
+    var heights = firstLine.split(" ").map( x => parseInt(x));
+    var word = secondLine;
 
+    console.log(designerPdfViewer(heights, word));
+    return  "Hola";
 };
 
 
+function designerPdfViewer(heights, word) {
+    var area = 0;
 
+    return area;
+}
