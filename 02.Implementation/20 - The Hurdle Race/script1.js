4ca820681d5bc94a98ab035220b7a6ce16fda5c6
Gustavo <consugus@gmail.com>
2018-05-21 13:01:44 -0300

    Implementation - 20-The Hurdle Race - Declaration

---
+++ 02.Implementation/20 - The Hurdle Race/script1.js
@@ -0,0 +1,16 @@
+//Input
+var firstLine = "5 4"; var secondLine = "1 6 3 5 2"; // 2
+var firstLine = "5 7"; var secondLine = "2 5 4 5 2"; // 0
+
+retorno = eval(firstLine, secondLine);
+
+//Solution
+function eval(firstLine, secondLine){
+
+};
+
+
+function hurdleRace(k, height) {
+
+
+}
