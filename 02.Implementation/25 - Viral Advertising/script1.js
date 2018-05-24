9e971cf8dcad439c1c242a1af88417539dea04fd
Gustavo <consugus@gmail.com>
2018-05-24 17:31:17 -0300

    implementation - 25-Viral Advertising - Declaration

---
+++ 02.Implementation/25 - Viral Advertising/script1.js
@@ -0,0 +1,19 @@
+//Input
+var firstLine = "3"; // 9
+
+retorno = eval(firstLine);
+
+//Solution
+function eval(firstLine){
+
+    console.log( viralAdvertising(parseInt(firstLine)) );
+    return "Hola";
+};
+
+
+function viralAdvertising(n) {
+    var count = 0;
+
+
+    return count;
+}
