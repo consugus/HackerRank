e35285ad0826ee488b467dcc23d6177227b9255e
Gustavo <consugus@gmail.com>
2018-05-24 11:17:33 -0300

    Implementation - 22-Utopian Tree - Solved

--- 02.Implementation/22 - Utopian Tree/script1.js
+++ 02.Implementation/22 - Utopian Tree/script1.js
@@ -1,5 +1,6 @@
 //Input
-var firstLine = "3"; var secondLine = "0 1 4";
+var firstLine = "11"; var secondLine = "0 1 2 3 4 5 6 7 8 9 10";
+// 1,2,3,6,7,14,15,30,31,62,63
 
 retorno = eval(firstLine, secondLine);
 
@@ -16,5 +17,17 @@ function eval(firstLine, secondLine){
 function utopianTree(n) {
     var result = 0;
 
+    // taken from The On-line Encyclopedia of Integer Sequences
+    if(n == 0){ return 1;};
+
+    // One way
+    result = (1 + n%2) *utopianTree(n-1) + 1 - n%2;
     return result;
+
+    // Another way
+    // if((n%2) == 0){
+    //     result = 2^(n/2+1)-1;
+    // } else{
+    //     result = 2^(1+(n+1)/2)-2;
+    // };
 }
