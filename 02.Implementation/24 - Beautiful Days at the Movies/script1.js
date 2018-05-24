2a3a234666fd0f479d7d5d7f53cf78667d635589
Gustavo <consugus@gmail.com>
2018-05-24 17:19:30 -0300

    Implementation - 24 - Beautiful Days a the Movies - Solved

--- 02.Implementation/24 - Beautiful Days at the Movies/script1.js
+++ 02.Implementation/24 - Beautiful Days at the Movies/script1.js
@@ -12,10 +12,27 @@ function eval(firstLine){
 };
 
 
-function beautifulDays(i, j, k) {
+function beautifulDays(start, end, k) {
     var count = 0;
-
-
-
+    for(var i = start ; i <= end ; i++){
+        var tmpDirect = i;
+        var tmpReverse = reverseNumber(i);
+        //console.log("\ntmpDirect: " + tmpDirect + "\ttmpReverse: " + tmpReverse + "\n\n");
+        if(Math.abs(tmpDirect - tmpReverse)%k == 0){
+            count++;
+        };
+    };
     return count;
 }
+
+function reverseNumber(x){
+    var reversed = "";
+    var len = x.toString().length;
+    var str = x.toString();
+    for(var i = 0 ; i <  len ; i++){
+        reversed += str[len - i - 1];
+    };
+    var xReversed = parseInt(reversed);
+
+    return xReversed;
+};
