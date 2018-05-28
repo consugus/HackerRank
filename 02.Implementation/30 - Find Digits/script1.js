b76b927840c68f12c87158939e2af5be3791c0e0
Gustavo <consugus@gmail.com>
2018-05-28 12:28:11 -0300

    Implementation - 30-Find Digits - some advances

--- 02.Implementation/30 - Find Digits/script1.js
+++ 02.Implementation/30 - Find Digits/script1.js
@@ -1,5 +1,6 @@
 //Input
 var firstLine = "2"; var secondLine = ["12","1012"];
+var firstLine = "1"; var secondLine = ["1012"];
 
 retorno = eval(firstLine, secondLine);
 
@@ -20,7 +21,20 @@ function eval(firstLine, secondLine){
 
 function findDigits(n) {
     var divisor = 0;
+    var num = n.toString().split("");
+    var len = num.length;
+    console.log( "\n\nnum: " + num  + "\tlen: " + len);
+    var frecuencies = new Array(len + 2).join("0").split("").map(parseInt);if(len > 1){ frecuencies[1] = 0 };
+    for(var i = 0 ; i < len ; i++){
 
+        console.log( parseInt(num[i]) );
+
+        //if( parseInt(num[i] != 0) ){
+            frecuencies[parseInt(num[i]) + 1]++;
+        //};
+
+    };
+    console.log("frecuencies: " + frecuencies);
 
 
     return divisor;
