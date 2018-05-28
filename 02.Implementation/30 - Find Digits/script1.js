4ee10c2915771d09b8efa10ec995bb3ec3f8ad2d
Gustavo <consugus@gmail.com>
2018-05-28 16:29:47 -0300

    Implementation - 30-Find Digits - Solved

--- 02.Implementation/30 - Find Digits/script1.js
+++ 02.Implementation/30 - Find Digits/script1.js
@@ -1,6 +1,6 @@
 //Input
 var firstLine = "2"; var secondLine = ["12","1012"];
-var firstLine = "1"; var secondLine = ["1012"];
+// var firstLine = "1"; var secondLine = ["1012"];
 
 retorno = eval(firstLine, secondLine);
 
@@ -20,22 +20,19 @@ function eval(firstLine, secondLine){
 
 
 function findDigits(n) {
-    var divisor = 0;
+    var divisors = 0;
     var num = n.toString().split("");
     var len = num.length;
-    console.log( "\n\nnum: " + num  + "\tlen: " + len);
-    var frecuencies = new Array(len + 2).join("0").split("").map(parseInt);if(len > 1){ frecuencies[1] = 0 };
+    var frecuencies = new Array(10 + 1).join("0").split("").map(parseInt);if(len > 1){ frecuencies[1] = 0 };
     for(var i = 0 ; i < len ; i++){
-
-        console.log( parseInt(num[i]) );
-
-        //if( parseInt(num[i] != 0) ){
-            frecuencies[parseInt(num[i]) + 1]++;
-        //};
-
+        frecuencies[parseInt(num[i])]++;
     };
-    console.log("frecuencies: " + frecuencies);
 
+    for(var i = 0 ; i < 10 ; i++){
+        if( (n % i == 0) ){
+            divisors += 1 * frecuencies[i];
+        };
+    };
 
-    return divisor;
+    return divisors;
 }
