e2f19152e73a630abf74d36b5b9b0546c0a3feb1
Gustavo <consugus@gmail.com>
2018-05-29 17:16:46 -0300

    Imlementation - -Append and Delete - some advances

--- 02.Implementation/31 - Append and Delete/script1.js
+++ 02.Implementation/31 - Append and Delete/script1.js
@@ -1,13 +1,13 @@
 //Input
-var firstLIne = "hackerhappy"; var secondLine = "hackerrank"; var thirdLine = "9"; // YES
+var firstLine = "hackerhappy"; var secondLine = "hackerrank"; var thirdLine = "9"; // YES
 // var firstLIne = "aba"; var secondLine = "aba"; var thirdLine = "7"; // YES
 // var firstLIne = "ashley"; var secondLine = "ash"; var thirdLine = "2"; // No
 
-retorno = eval(firstLIne, secondLine, thirdLine);
+retorno = eval(firstLine, secondLine, thirdLine);
 
 //Solution
-function eval(firstLIne, secondLine, thirdLine){
-    var s = firstLIne; var t = secondLine;
+function eval(firstLine, secondLine, thirdLine){
+    var s = firstLine; var t = secondLine;
     var k = parseInt(thirdLine);
 
     console.log(appendAndDelete(s, t, k));
@@ -17,7 +17,22 @@ function eval(firstLIne, secondLine, thirdLine){
 
 function appendAndDelete(s, t, k) {
     console.log("s: " + s + "\tt: " + t + "\tk: " + k + "\n\n");
+    var common  = 0;
+    var firstIteration;
+    if(s.length < t.length){
+        firstIteration = s.length;
+    } else{
+        firstIteration = t.length;
+    };
 
+    for(var i = 0 ; i < firstIteration ; i++){
+        //console.log("s[" + i + "]: " + s[i] + "t[" + i + "]: " + t[i]);
+        if(s[i] != t[i]){
+            break;
+        };
+        common = i;
+    };
+    console.log("common: " + common + "\ts.length: " + s.length + "\tt.length: " + t.length + "\n\n");
 
 }
 
