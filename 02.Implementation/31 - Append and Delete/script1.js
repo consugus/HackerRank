5fcadc1ffbd5f6125cf11c0a4599803d071331d7
Gustavo <consugus@gmail.com>
2018-05-31 10:28:31 -0300

    Implementation - 31-Append and Delete - Solved

--- 02.Implementation/31 - Append and Delete/script1.js
+++ 02.Implementation/31 - Append and Delete/script1.js
@@ -1,7 +1,7 @@
 //Input
 var firstLine = "hackerhappy"; var secondLine = "hackerrank"; var thirdLine = "9"; // YES
-// var firstLIne = "aba"; var secondLine = "aba"; var thirdLine = "7"; // YES
-// var firstLIne = "ashley"; var secondLine = "ash"; var thirdLine = "2"; // No
+// var firstLine = "aba"; var secondLine = "aba"; var thirdLine = "7"; // YES
+// var firstLine = "ashley"; var secondLine = "ash"; var thirdLine = "2"; // No
 
 retorno = eval(firstLine, secondLine, thirdLine);
 
@@ -16,24 +16,27 @@ function eval(firstLine, secondLine, thirdLine){
 
 
 function appendAndDelete(s, t, k) {
-    console.log("s: " + s + "\tt: " + t + "\tk: " + k + "\n\n");
+    //console.log("s: " + s + "\tt: " + t + "\tk: " + k + "\n\n");
     var common  = 0;
-    var firstIteration;
-    if(s.length < t.length){
-        firstIteration = s.length;
-    } else{
-        firstIteration = t.length;
-    };
 
-    for(var i = 0 ; i < firstIteration ; i++){
+    for(var i = 0 ; i < (Math.min(s.length, t.length))  ; i++){
         //console.log("s[" + i + "]: " + s[i] + "t[" + i + "]: " + t[i]);
         if(s[i] != t[i]){
             break;
         };
         common = i;
     };
-    console.log("common: " + common + "\ts.length: " + s.length + "\tt.length: " + t.length + "\n\n");
+    //console.log("common: " + common + "\ts.length: " + s.length + "\tt.length: " + t.length + "\n\n");
 
-}
+    if( (s.length + t.length - 2 - 2*common) > k ){
+        return "No";
+    } else if( (s.length + t.length - 2*common)%2 == k%2 ){
+        return "Yes";
+    } else if( (s.length + t.length - k) < 0 ){
+        return "Yes";
+    } else{
+        return "No";
+    };
+};
 
 
