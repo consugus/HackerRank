cd225f03ac3970d03127b06d407255871e024036
Gustavo <consugus@gmail.com>
2018-05-31 12:52:15 -0300

    Implementation - 33 - Library Fine - Solved

--- 02.Implementation/33 - Library Fine/script1.js
+++ 02.Implementation/33 - Library Fine/script1.js
@@ -1,33 +1,39 @@
 //Input
-var firstLine = "9 6 2015"; var secondLine = "6 6 2015";
+
+// actual / expected
+var firstLine = "9 6 2015"; var secondLine = "6 6 2015"; // 45
+// var firstLine = "2 7 1014"; var secondLine = "1 1 1015"; // 0
+
+
 
 retorno = eval(firstLine, secondLine);
 
 //Solution
 function eval(firstLine, secondLine){
     var tmp = firstLine.split(" ").map( x => parseInt(x));
-    var actual = {
-        d: tmp[0],
-        m: tmp[1],
-        y: tmp[2]
-    };
+    var d1 =  tmp[0], m1 = tmp[1], y1=  tmp[2];
     tmp = secondLine.split(" ").map( x => parseInt(x));
-    var expected = {
-        d: tmp[0],
-        m: tmp[1],
-        y: tmp[2]
-    };
-    // console.log(libraryFine(d1, m1, y1, d2, m2, y2));
-    console.log(libraryFine(actual, expected));
+    var d2 = tmp[0], m2 = tmp[1], y2 = tmp[2];
+
+    console.log(libraryFine(d1, m1, y1, d2, m2, y2));
     return "Hola";
 };
 
 
-function libraryFine(actual, expected) {
-    console.log("actual Date: " + actual.d + "/" + actual.m + "/" + actual.y);
-    console.log("expected Date: " + expected.d + "/" + expected.m + "/" + expected.y);
+function libraryFine(d1, m1, y1, d2, m2, y2) {
     var fine = 0;
 
-
+    if( (y1 < y2 || (y1 == y2 && m1 < m2) || (y1 == y2 && m1 == m2 && d1 < d2)) ){
+        return fine;
+    };
+    if( (y1 - y2) > 0 ){
+        return 10000;
+    };
+    if( (m1 - m2) > 0 ){
+        return ( (m1 - m2) * 500);
+    };
+    if( (d1 - d2) > 0 ){
+        return ( (d1 - d2) * 15);
+    };
     return fine;
 }
