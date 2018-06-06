09f8c489f0cfcd344a661be49d8ea877f5e1b78d
Gustavo <consugus@gmail.com>
2018-06-06 16:01:19 -0300

    Implementation - 39-Queen's Attack - Solved

--- 02.Implementation/39 - Queen's Attack II/script1.js
+++ 02.Implementation/39 - Queen's Attack II/script1.js
@@ -1,7 +1,10 @@
 //Input
-var firstLine = "4 0"; var secondLine = "4 4"; var thirdLine = []; // 9
+// var firstLine = "4 0"; var secondLine = "4 4"; var thirdLine = []; // 9
 // var firstLine = "5 3"; var secondLine = "4 3"; var thirdLine = [[5, 5], [4, 2], [2, 3]]; // 10
+var firstLine = "5 3"; var secondLine = "4 3"; var thirdLine = [[5, 5], [4, 2], [2, 3]]; // 10
 // var firstLine = "1 0"; var secondLine = "1 1"; var thirdLine = []; // 0
+var firstLine = "100000 0"; var secondLine = "4187 5068"; var thirdLine = []; // 308369
+
 
 retorno = eval(firstLine, secondLine, thirdLine);
 
@@ -20,53 +23,163 @@ function eval(firstLine, secondLine, thirdLine){
 
 
 function queensAttack(n, k, r_q, c_q, obstacles) {
-    var positionsAttacked = 0;
+    var posAttacked = 0;
+    var hashObstacles = new Array; // ((n*n) + 1);
 
-    positionsAttacked += upForwards(n, k, r_q, c_q, obstacles);
-    positionsAttacked += downForwards(n, k, r_q, c_q, obstacles);
-    positionsAttacked += upBackwards(n, k, r_q, c_q, obstacles);
-    positionsAttacked += downBackwards(n, k, r_q, c_q, obstacles);
-    positionsAttacked += rowForwards(n, k, r_q, c_q, obstacles);
-    positionsAttacked += rowBackwards(n, k, r_q, c_q, obstacles);
-    positionsAttacked += colUp(n, k, r_q, c_q, obstacles);
-    positionsAttacked += colDown(n, k, r_q, c_q, obstacles);
+    for(var i = 0 ; i < obstacles.length ; i++){
+        var loc = createHash(n, obstacles[i][0], obstacles[i][1]);
+        hashObstacles[loc] = 1;
+    };
 
-    return positionsAttacked;
+    posAttacked += upForwards(   n, k, r_q, c_q, hashObstacles);
+    posAttacked += upBackwards(  n, k, r_q, c_q, hashObstacles);
+    posAttacked += downForwards( n, k, r_q, c_q, hashObstacles);
+    posAttacked += downBackwards(n, k, r_q, c_q, hashObstacles);
+    posAttacked += rowForwards(  n, k, r_q, c_q, hashObstacles);
+    posAttacked += rowBackwards( n, k, r_q, c_q, hashObstacles);
+    posAttacked += colUp(        n, k, r_q, c_q, hashObstacles);
+    posAttacked += colDown(      n, k, r_q, c_q, hashObstacles);
+
+    return posAttacked;
 }
 
-function upForwards(n, k, r_q, c_q, obstacles){
+function createHash(n, row, col){
+    var tempRow = "", tempCol = "";
 
-    return 1;
+    if(row.toString().length < n.toString().length){
+        for(var i = 0 ; i < (n.toString().length - row.toString().length) ; i++){
+            tempRow += "0";
+        };
+    };
+    tempRow += row.toString();
+
+    if(col.toString().length < n.toString().length){
+        for(var i = 0 ; i < (n.toString().length - col.toString().length) ; i++){
+            tempCol += "0";
+        };
+    };
+    tempCol += col.toString();
+
+    // console.log( tempRow.toString() + tempCol.toString() );
+    return parseInt(tempRow.toString() + tempCol.toString());
 };
 
-function upBackwards(n, k, r_q, c_q, obstacles){
+function upForwards(n, k, r_q, c_q, hashObstacles){
+    var count = 0;
+    for(var i = 1 ; i < n ; i++){
+        if( (r_q+i) > n || (c_q+i) > n ){
+            break;
+        };
 
-    return 1;
+        if( hashObstacles[createHash( n, (r_q + i), (c_q + i) ) ] === 1){
+            break;
+        };
+        count++;
+    };
+    return count;
 };
 
-function downForwards(n, k, r_q, c_q, obstacles){
+function upBackwards(n, k, r_q, c_q, hashObstacles){
+    var count = 0;
+    for(var i = 1 ; i < n ; i++){
+        if( (r_q+i) > n || (c_q-i) < 1 ){
+            break;
+        };
 
-    return 1;
+        if( hashObstacles[createHash( n, (r_q + i), (c_q - i) ) ] === 1){
+            break;
+        };
+        count++
+    };
+    return count;
 };
 
-function downBackwards(n, k, r_q, c_q, obstacles){
+function downForwards(n, k, r_q, c_q, hashObstacles){
+    var count = 0;
+    for(var i = 1 ; i < n ; i++){
+        if( (r_q-i) < 1 || (c_q+i) > n ){ //(r_q+i) > n || (c_q+i) > n || (r_q-i) < 1 || (c_q-i) < 1
+            break;
+        };
 
-    return 1;
+        if( hashObstacles[createHash( n, (r_q - i), (c_q + i) ) ] === 1){
+            break;
+        };
+        count++;
+    };
+    return count;
 };
 
-function rowForwards(n, k, r_q, c_q, obstacles){
+function downBackwards(n, k, r_q, c_q, hashObstacles){
+    var count = 0;
+    for(var i = 1 ; i < n ; i++){
+        if( (r_q-i) < 1 || (c_q-i) < 1 ){
+            break;
+        };
 
-    return 1;
+        if( hashObstacles[createHash( n, (r_q - i), (c_q - i) ) ] === 1){
+            break;
+        };
+        count++;
+    };
+    return count;
 };
 
-function rowBackwards(n, k, r_q, c_q, obstacles){
+function rowForwards(n, k, r_q, c_q, hashObstacles){
+    var count = 0;
+    for(var i = 1 ; i < n ; i++){
+        if( (c_q+i) > n ){
+            break;
+        };
 
-    return 1;
+        if( hashObstacles[createHash( n, (r_q), (c_q + i) ) ] === 1){
+            break;
+        };
+        count++;
+    };
+    return count;
 };
 
-function colUp(n, k, r_q, c_q, obstacles){
+function rowBackwards(n, k, r_q, c_q, hashObstacles){
+    var count = 0;
+    for(var i = 1 ; i < n ; i++){
+        if( (c_q-i) < 1 ){
+            break;
+        };
 
-    return 1;
+        if( hashObstacles[createHash( n, (r_q), (c_q - i) ) ] === 1){
+            break;
+        };
+        count++;
+    };
+    return count;
 };
 
-function colDown(n, k, r_q, c_q, obstacles){    return 1;};
+function colUp(n, k, r_q, c_q, hashObstacles){
+    var count = 0;
+    for(var i = 1 ; i < n ; i++){
+        if( (r_q+i) > n ){
+            break;
+        };
+
+        if( hashObstacles[createHash( n, (r_q + i), (c_q) ) ] === 1){
+            break;
+        };
+        count++;
+    };
+    return count;
+};
+
+function colDown(n, k, r_q, c_q, hashObstacles){
+    var count = 0;
+    for(var i = 1 ; i < n ; i++){
+        if( (r_q-i) < 1 ){
+            break;
+        };
+
+        if( hashObstacles[createHash( n, (r_q - i), (c_q) ) ] === 1){
+            break;
+        };
+        count++;
+    };
+    return count;
+};
