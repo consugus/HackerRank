3ba961dace8dea1eb59e797109c7c1fdd941b746
Gustavo <consugus@gmail.com>
2018-06-16 00:59:14 -0300

    Implementation - 46-Beautiful Triplets - Declaration

---
+++ 02.Implementation/46 - Beautiful Triplets/script1.js
@@ -0,0 +1,22 @@
+//Input
+var firstLine = "7 3"; var secondLine = "1 2 4 5 7 8 10";
+
+retorno = eval(firstLine, secondLine);
+
+//Solution
+function eval(firstLine, secondLine){
+    var nd = firstLine.split(" ").map( x => parseInt(x));
+    var n = nd[0], d = nd[1];
+    var arr = secondLine.split(" ").map( x => parseInt(x) );
+    console.log(beautifulTriplets(d, arr));
+    return "Hola";
+};
+
+
+function beautifulTriplets(d, arr) {
+    var numberOfBeautifulTriplets = 0;
+
+
+
+    return numberOfBeautifulTriplets;
+}
