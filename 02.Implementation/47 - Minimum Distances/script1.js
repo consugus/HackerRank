316181e3561ee4af46fe4a582b89c266ad24728d
Gustavo <consugus@gmail.com>
2018-06-17 20:56:25 -0300

    Implementation - 47-Minimum Distances - Solved

--- 02.Implementation/47 - Minimum Distances/script1.js
+++ 02.Implementation/47 - Minimum Distances/script1.js
@@ -1,5 +1,6 @@
 //Input
 var firstLine = "6"; var secondLine = "7 1 3 4 1 7"; // 3
+var firstLine = "5"; var secondLine = "1 2 3 4 10"; // -1
 
 retorno = eval(firstLine, secondLine);
 
@@ -14,8 +15,19 @@ function eval(firstLine, secondLine){
 
 
 function minimumDistances(a) {
-    var minDist = 0, len = a.length;
+    var len = a.length;
+    var minDist = Number.MAX_SAFE_INTEGER;
+    var hashA = [];
 
+    for(var i = 0 ; i < len ; i++){
+        if(hashA[a[i]] == undefined   || hashA[a[i]] == -1 ){
+            hashA[a[i]] = i;
+        }   else{
+            if( Math.abs( hashA[a[i]] - i) < minDist ){
+                minDist =  Math.abs( hashA[a[i]] - i);
+            };
+        };
+    };
 
-    return minDist;
+    return  (minDist == Number.MAX_SAFE_INTEGER) ? -1: minDist
 }
