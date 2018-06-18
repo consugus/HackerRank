1a7448ec063a2990a9c2b6d43aace22c1a140a9f
Gustavo <consugus@gmail.com>
2018-06-17 21:42:56 -0300

    Implementation - 48-Halloween Sale - Solved

--- 02.Implementation/48 - Halloween Sale/script1.js
+++ 02.Implementation/48 - Halloween Sale/script1.js
@@ -1,6 +1,7 @@
 //Input
 var firstLine = "20 3 6 80"; // 6
-var firstLine = "20 3 6 85"; // 7
+// var firstLine = "20 3 6 85"; // 7
+var firstLine = "16 2 1 9981"; // 9917
 
 retorno = eval(firstLine);
 
@@ -8,7 +9,7 @@ retorno = eval(firstLine);
 function eval(firstLine){
     var pdms = firstLine.split(" ").map( x => parseInt(x) );
     var p = pdms[0], d = pdms[1], m = pdms[2], s = pdms[3];
-    howManyGames(p, d, m, s);
+    console.log(howManyGames(p, d, m, s));
     return "Hola";
 };
 
@@ -16,7 +17,18 @@ function eval(firstLine){
 function howManyGames(p, d, m, s) {
     // Return the number of games you can buy
     var numberOfGames = 0;
+    var nextPrice = p;
 
+    while(s >= p){
+        numberOfGames++;
+        s = s - p;
+
+        if( (p - d) < m ){
+            p = m;
+        } else{
+            p = p - d;
+        };
+    };
 
     return numberOfGames;
 }
