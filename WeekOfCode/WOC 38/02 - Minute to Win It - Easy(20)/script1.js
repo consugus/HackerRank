0863f18a1389fdff54cb87e94ef470e5ba6a0337
Gustavo <consugus@gmail.com>
2018-06-19 10:05:47 -0300

    WOC 38 - 02 =-Minute To Win It - Declaration

--- WeekOfCode/WOC 38/02 - Minute to Win It - Easy(20)/script1.js
+++ WeekOfCode/WOC 38/02 - Minute to Win It - Easy(20)/script1.js
@@ -1,12 +1,22 @@
 //Input
+var firstLine = "6 2", secondLine = "1 2 5 7 9 85"; // 2
+var firstLine = "6 2", secondLine = "1 2 5 7 9 85"; // 2
 
-
-retorno = eval();
+retorno = eval(firstLine, secondLine);
 
 //Solution
-function eval(){
-
+function eval(firstLine, secondLine){
+    var nk = firstLine.split(" ").map( x => parseInt(x) );
+    var n = nk[0], k = nk[1];
+    var a = secondLine.split(" ").map( x => parseInt(x) );
+    console.log(minuteToWinIt(a, k));
+    return "Hola";
 };
 
 
+function minuteToWinIt(a, k) {
+    // Return the minimum amount of time in minutes.
 
+
+    return count;
+}
