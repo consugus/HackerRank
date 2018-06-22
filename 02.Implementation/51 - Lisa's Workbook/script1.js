c9fae8a1570c40dd5fe7c0681681afcdeb50fe56
Gustavo <consugus@gmail.com>
2018-06-22 14:02:17 -0300

    Implementation - 51 - Lisa's Workbook - Solved

--- 02.Implementation/51 - Lisa's Workbook/script1.js
+++ 02.Implementation/51 - Lisa's Workbook/script1.js
@@ -6,8 +6,9 @@ retorno = eval(firstLine, secondLine);
 //Solution
 function eval(firstLine, secondLine){
     var nk = firstLine.split(" ").map( x => parseInt(x) );
-    var n = nk[0], k = nk[1];
-    var arr = secondLine.split(" ").map( x => parseInt(x) );
+    var n = nk[0]; // number of chapters
+    var k = nk[1]; // maximum number of problems per page
+    var arr = secondLine.split(" ").map( x => parseInt(x) ); // problems in the ith chapter
 
     console.log(workbook(n, k, arr));
     return "Hola";
@@ -16,7 +17,18 @@ function eval(firstLine, secondLine){
 
 function workbook(n, k, arr) {
     var specialProblemsCount = 0;
-
+    var len = arr.length;
+    var page = 1;
+    for(var i = 0 ; i < len ; i++){
+        for(var j = 1 ; j <= arr[i] ; j++){
+            if(j == page){
+                specialProblemsCount++;
+            };
+            if(j % k == 0 || j == arr[i]){
+                page++;
+            };
+        };
+    };
 
     return specialProblemsCount;
 }
