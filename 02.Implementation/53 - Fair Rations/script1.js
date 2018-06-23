30482b0869b4490dbaa0e49031bd979e655f90f3
Gustavo <consugus@gmail.com>
2018-06-23 18:44:29 -0300

    Implementation - 53-Fair Rations - Solved

--- 02.Implementation/53 - Fair Rations/script1.js
+++ 02.Implementation/53 - Fair Rations/script1.js
@@ -1,6 +1,7 @@
 //Input
 var firstLine =  "5", secondLine = "2, 3, 4, 5, 6"; // 4
-var firstLine =  "2", secondLine = "1 2"; //  NO
+// var firstLine =  "2", secondLine = "1 2"; //  NO
+var firstLine =  "4", secondLine = "2 3 4 5"; //  2
 
 retorno = eval(firstLine, secondLine);
 
@@ -14,9 +15,31 @@ function eval(firstLine, secondLine){
 
 
 function fairRations(B) {
-    var minNum = 0;
+    console.log("0.\tB: " + B);
+    var minNum = 0, len = B.length;
+    // If count of odd numbers is odd the problem doesn't have solution
 
+    // Count of odd numbers
+    var odds = 0;
+    for(var i = 0 ; i < len ; i++){
+        if(B[i]%2 != 0){ odds++; };
+    };
+    if(odds%2 == 1){ return "NO"; };
 
+    // Counting minNum
+    if(B[0]%2 != 0){
+        B[0]++;
+        B[1]++;
+        minNum +=2;
+    };
+    for(var i = 1 ; i < len -1; i++){
+        if(B[i]%2 != 0){
+            B[i]++;
+            B[i+1]++;
+            minNum +=2;
+        };
+        console.log(i + ".\tB: " + B);
+    };
 
-    return minNum
+    return minNum;
 }
