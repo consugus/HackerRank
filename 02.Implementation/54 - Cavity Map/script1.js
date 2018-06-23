f48e85349f2c30e58bdc460ac721b8c73b162e80
Gustavo <consugus@gmail.com>
2018-06-23 19:09:09 -0300

    Implementation ; 54-Cavity Map - Declaration

---
+++ 02.Implementation/54 - Cavity Map/script1.js
@@ -0,0 +1,31 @@
+//Input
+
+var firstLine = "4", secondLine = ["1112", "1912", "1892", "1234"]; // "1112", "1X12", "18X2", "1234"
+"1112", "1X12", "18X2", "1234"
+
+retorno = eval(firstLine, secondLine);
+
+//Solution
+function eval(firstLine, secondLine){
+    var n = parseInt(firstLine), grid = [];
+    for(var i = 0 ; i < n ; i++){
+        grid[i] = secondLine[i].split("").map( x => parseInt(x) );
+    };
+    console.log(cavityMap(grid));
+    return "Hola";
+};
+
+
+function cavityMap(grid) {
+    var len = grid.length, str = "";
+
+    console.log("grid");
+    for(var i = 0 ; i < len ; i++){
+        console.log(i + ".\t" + grid[i]);
+    };
+
+    
+
+
+    return str;
+}
