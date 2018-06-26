e0c05c37878fe5fc73bc04da23009bc0d5b4ab9e
Gustavo <consugus@gmail.com>
2018-06-26 14:20:10 -0300

    Implementation - 58-Strange Counter - Declaration

---
+++ 02.Implementation/58 - Strange Counter/script1.js
@@ -0,0 +1,20 @@
+//Input
+var firstLine = "4"; // 6
+
+retorno = eval(firstLine);
+
+//Solution
+function eval(firstLine){
+    var t = parseInt(firstLine);
+    console.log(strangeCounter(t));
+    return "Hola";
+};
+
+
+function strangeCounter(t) {
+    var display = 6;
+
+
+
+    return display;
+}
