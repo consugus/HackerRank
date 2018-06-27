ee7d53ad57d8246df98671f438152180826d086d
Gustavo <consugus@gmail.com>
2018-06-26 22:48:32 -0300

    Implementation - 58 - Strange Counter - Some advances

--- 02.Implementation/58 - Strange Counter/script1.js
+++ 02.Implementation/58 - Strange Counter/script1.js
@@ -12,9 +12,29 @@ function eval(firstLine){
 
 
 function strangeCounter(t) {
-    var display = 6;
+    var display = 0, value = 3, startTime = 1, endTime = startTime+value-1;
+    console.log("t: " + t + "\n\n");
+    console.log("Primera Vez: value: " + value + "\tstartTime: " + startTime + "\tendTime: " + endTime);
+    console.log( "Es true el while?: " + (startTime < t) + "\n\n");
 
+    while(startTime < t){
+        value = 2*value;
+        startTime = endTime + 1;
+        endTime = startTime+value-1;
+        console.log("value: " + value + "\tstartTime: " + startTime + "\tendTime: " + endTime);
+    };
 
+    for(var i = startTime ; i <= endTime ; i++ ){
+        if(startTime == t){
+            console.log("entró acá");
+            display = value;
+            break;
+        } else{
+            value--;
+            startTime++;
+        };
+        console.log("i: " + i);
+    };
 
     return display;
 }
