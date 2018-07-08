3233c18c1d73c393777e922c6d2cdd01883f50ba
Gustavo <consugus@gmail.com>
2018-07-07 23:51:00 -0300

    IPK - 2d Array - DS - DEclaration and Solved

--- Interview Preparation Kit/Arrays/02 - 2D Array - DS/script1.js
+++ Interview Preparation Kit/Arrays/02 - 2D Array - DS/script1.js
@@ -1,12 +1,38 @@
 //Input
+var firstLine = ["-9 -9 -9 1 1 1", "0 -9 0 4 3 2", "-9 -9 -9 1 2 3", "0 0 8 6 6 0", "0 0 0 -2 0 0", "0 0 1 2 4 0"];
+// 28
+var firstLine = ["1 1 1 0 0 0", "0 1 0 0 0 0", "1 1 1 0 0 0", "0 0 2 4 4 0", "0 0 0 2 0 0", "0 0 1 2 4 0"];
+// 19
 
-
-retorno = eval();
+retorno = eval(firstLine);
 
 //Solution
-function eval(){
+function eval(firstLine){
+    var arr = [];
+    for(var i = 0 ; i < 6 ; i++){
+        var tmp = firstLine[i].split(" ").map( x => parseInt(x) );
+        arr.push(tmp);
+    };
 
+    console.log(hourglassSum(arr));
+    return "Hola";
 };
 
 
+function hourglassSum(arr) {
+    var maxHourglass = Number.MIN_SAFE_INTEGER;
+    for(var i = 0 ; i < 6 ; i++){
+        console.log(arr[i]);
+    };
 
+    for(var i = 0 ; i <= 3 ; i++){
+        for(var j = 0 ; j <= 3 ; j++){
+            var tmp = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            if(tmp > maxHourglass){
+                maxHourglass = tmp;
+            };
+        };
+    };
+
+    return maxHourglass;
+}
