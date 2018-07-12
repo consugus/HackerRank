601dc280c8a12174cfddf254c6b7fee653135766
Gustavo <consugus@gmail.com>
2018-07-12 10:30:26 -0400

    Data Structures-Arrays - 02-Sparse Arrays - Declaration

---
+++ 13.Data Structures/01. Arrays/02 - Sparse Arrays/script1.js
@@ -0,0 +1,18 @@
+//Input
+var firstLine = "4", secondLine = ["aba", "baba", "aba", "xzxb"], thirdLine = "3", fourthLine = ["aba", "xzxb", "ab"];
+var firstLine = "3", secondLine = ["def", "de", "fgh"], thirdLine = "3", fourthLine = ["de", "lmn", "fgh"];
+var firstLine = "13", secondLine = ["abcde", "sdaklfj", "asdjf", "na", "basdn", "sdaklfj", "asdjf", "na", "asdjf", "na", "basdn", "sdaklfj", "asdjf"], thirdLine = "5", fourthLine = ["abcde", "sdaklfj", "asdjf", "na", "basdn"];
+
+retorno = eval(firstLine, secondLine, thirdLine, fourthLine);
+
+//Solution
+function eval(firstLine, secondLine, thirdLine, fourthLine){
+
+    return "Hola";
+};
+
+function matchingStrings(strings, queries) {
+
+
+}
+
