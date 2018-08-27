485d3c8f6eca4ec1e2c02322207b7e938f4dbc95
Gustavo <consugus@gmail.com>
2018-08-27 16:53:05 -0300

    Data Structure - Arrays - Dinamic Array - Solved

--- 13.Data Structures/01. Arrays/04 - Dinamic Array/script1.js
+++ 13.Data Structures/01. Arrays/04 - Dinamic Array/script1.js
@@ -23,13 +23,13 @@ retorno = evaluate(contenido);
 
 function evaluate(contenido){
     "use strict";
-    console.log(contenido + "\n\n");
+    // console.log(contenido + "\n\n");
     var n = 1; var queries = "";
 
     var lines = contenido.split("\n");
     var firstLine = lines[0].split(" ").map( x => parseInt(x));
     var n = firstLine[0]; var q = firstLine[1];
-    console.log("n: " + n + "\tq: " + q + "\n\n");
+    // console.log("n: " + n + "\tq: " + q + "\n\n");
 
     var queries = [];
     for(var i = 1 ; i <= q ; i++){
@@ -45,33 +45,35 @@ function evaluate(contenido){
 
 
 function dynamicArray(n, queries) {
-    var lastAnswer = 0, len = queries.length;
-    var lastAnswers = [];
-    var seqList = new Array (n); console.log("seqList: " + seqList);
-    for(var i = 0 ; i < n ; i++){
-        seqList[i] = [];
-    };
 
-    for(var i = 0 ; i < len ; i++){
-        switch(queries[i][0]){
+    var sequences = new Array (n);
+    var lastAnswers = [];
+    for(var i = 0 ; i < n ; i++){
+        sequences[i]  = [];
+    };
+    var lastAns = 0;
+
+    for(var i = 0; i < queries.length; i++) {
+        var [type, idx, value] = queries[i];
+
+        var sequenceIdx = (idx ^ lastAns) % n;
+        var sequence = sequences[sequenceIdx];
+
+        switch(type){
             case 1: {
-                // console.log("i: " + i + "\tconsulta 1");
-                seqList[queries[i][1]].push(queries[i][2]);
+                sequence.push(value);
                 break;
             };
             case 2: {
-                //console.log("i: " + i + "\tconsulta 2");
-                var tmp = (queries[i][1] ^ lastAnswer) % 2;
-                lastAnswer = seqList[tmp][seqList[tmp].length-1];
-                // console.log("lastAnswer: " + lastAnswer);
-                lastAnswers.push(lastAnswer);
+                var elementIdx = value % sequence.length;
+                lastAns = sequence[elementIdx];
+                lastAnswers.push(lastAns);
+
                 break;
             };
         };
     };
-
     return lastAnswers;
-}
-
+};
 
 
