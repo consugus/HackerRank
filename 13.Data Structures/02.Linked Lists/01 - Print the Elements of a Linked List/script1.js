dc7aa8ac99c3f081f03d117cccc5e106f164cd03
Gustavo <consugus@gmail.com>
2018-08-27 13:16:49 -0300

    Data Structures - Arrays - Dinamic Arrays - Declaration and some advances

---
+++ 13.Data Structures/02.Linked Lists/01 - Print the Elements of a Linked List/script1.js
@@ -0,0 +1,28 @@
+//Input
+
+//          Syncronous mode (deprecated)
+// var archivoTxt = new XMLHttpRequest();      // objet that requests data from the server
+// archivoTxt.open("GET", "datos.txt", false);   // specifies the type of request; parameters:  Method, path/file, Async
+// archivoTxt.send();                          // execute the request
+// var contenido = archivoTxt.responseText;
+// console.log("contenido: " + contenido);     // display the response
+
+
+//          Asyncronous mode
+var archivoTxt = new XMLHttpRequest();      // objet that requests data from the server
+archivoTxt.open("GET", "datos.txt", true);   // specifies the type of request; parameters:  Method, path/file, Async
+archivoTxt.send();                          // execute the request
+archivoTxt.onreadystatechange = function(){
+        var contenido = archivoTxt.responseText;
+        console.log("contenido: " + contenido);     // display the response
+};
+
+retorno = eval();
+
+//Solution
+function eval(){
+
+};
+
+
+
