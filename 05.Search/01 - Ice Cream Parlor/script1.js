b9323491a5d0a749decd3db06d3ee3b9ac37e692
Gustavo <consugus@gmail.com>
2018-08-27 18:36:24 -0300

    Search - Ice Cream Parlor - Solved

--- 05.Search/01 - Ice Cream Parlor/script1.js
+++ 05.Search/01 - Ice Cream Parlor/script1.js
@@ -2,7 +2,7 @@
 
 //          Syncronous mode (deprecated)
 var archivoTxt = new XMLHttpRequest();      // objet that requests data from the server
-archivoTxt.open("GET", "TestCases/TestCase00-Input.txt", false);   // specifies the type of request; parameters:  Method, path/file, Async
+archivoTxt.open("GET", "TestCases/TestCase01-Input.txt", false);   // specifies the type of request; parameters:  Method, path/file, Async
 archivoTxt.send();                          // execute the request
 var contenido = archivoTxt.responseText;
 // console.log("contenido:\n" + contenido);     // display the response
@@ -22,26 +22,28 @@ retorno = evaluate(contenido);
 
 //Solution
 function evaluate(contenido){
-
     "use stritc";
     contenido = contenido.split("\n");
-    // console.log("contenido:\n" + contenido);
     let queriesNum = parseInt(contenido[0]);
     const queries = new Array(2);
-    // console.log("queries: " + queries);
-    for(var i = 1 ; i < contenido.length ; i+=3){
-        var m = contenido[i], arr = contenido[i+2];
+    var len = contenido.length;
+    for(var i = 1 ; i < len ; i+=3){
+        var m = contenido[i], arr = contenido[i+2].split(" ").map( x => parseInt(x) );
         console.log(icecreamParlor(m, arr));
     };
 };
 
 function icecreamParlor(m, arr) {
-    console.log("money: " + m + "\tflavors: " + arr);
-    var indexes = "0", len = arr.length;
+    var indexes = [0,0], len = arr.length;
 
-    for(var i = 0 ; i < len ; i++){
-
+    let flavor1 = 0, flavor2 = 0
+    for(var i = 0 ; i < len - 1 ; i++){ // flavor 1
+        for( var j = i + 1 ; j < len ; j++){ // flavor 2
+            if( (arr[i] + arr[j]) == m){
+                flavor1 = i + 1 ; flavor2 = j + 1;
+            };
+        };
     };
 
-    return indexes;
+    return flavor1 + " " +  flavor2;
 }
