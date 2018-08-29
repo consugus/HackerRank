be2a30e7bb6846ab0e7b9e06699e30ade33b7517
Gustavo <consugus@gmail.com>
2018-08-28 23:30:36 -0300

    Bit Manipulation - 06-Flipping bits - Solved

---
+++ 10.Bit Manipulation/06 - Flipping bits/script1.js
@@ -0,0 +1,44 @@
+//Input
+
+// #region          Syncronous mode (deprecated)
+var archivoTxt = new XMLHttpRequest();      // objet that requests data from the server
+archivoTxt.open("GET", "TestCases/TestCaseA-Input.txt", false);   // specifies the type of request; parameters:  Method, path/file, Async
+archivoTxt.send();                          // execute the request
+var contenido = archivoTxt.responseText;
+// console.log("contenido:\n" + contenido);     // display the response
+// #endregion
+
+// #region         Asyncronous mode
+// var archivoTxt = new XMLHttpRequest();      // objet that requests data from the server
+// archivoTxt.open("GET", "datos.txt", true);   // specifies the type of request; parameters:  Method, path/file, Async
+// archivoTxt.send();                          // execute the request
+// archivoTxt.onreadystatechange = function(){
+//         var contenido = archivoTxt.responseText;
+//         console.log("contenido: " + contenido);     // display the response
+// };
+// #endregion
+
+retorno = eval(contenido);
+
+//Solution
+function eval(contenido){
+    contenido = contenido.split("\n").map( x => parseInt(x) );
+    var len = parseInt(contenido.shift());
+    // console.log("queries: " + len + "\tcontenido: " + contenido);
+    var queries = contenido;
+    for(var i = 0 ; i < len ; i++){
+        console.log(flippingBits(queries[i]));
+    };
+
+};
+
+
+function flippingBits(N) {
+    var biggestNumber = 0;
+    for (var i = 0 ; i < 32 ; i++){
+        biggestNumber += Math.pow(2,i);
+    };
+    // console.log("biggestNumber: " + biggestNumber);
+
+    return (biggestNumber-N);
+}
