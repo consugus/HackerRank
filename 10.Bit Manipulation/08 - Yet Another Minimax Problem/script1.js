54b3bb7f237983e78dac8fe8ab27a0f4664fe979
Gustavo <consugus@gmail.com>
2018-09-05 13:42:30 -0300

    Bit Manipulation - 08-Yet Another Minimax Problem - Can't get to a rigth solution. I've wasted too much time so far, so I decide to move on a quit this excercise

--- 10.Bit Manipulation/08 - Yet Another Minimax Problem/script1.js
+++ 10.Bit Manipulation/08 - Yet Another Minimax Problem/script1.js
@@ -19,6 +19,8 @@
 // #endregion
 
 var firstLine = "4", secondLine = "1 2 3 4";
+var firstLine = "3", secondLine = "1 2 3";
+// var firstLine = "5", secondLine = "12 8 9 11 14";
 
 retorno = eval(firstLine, secondLine);
 
@@ -26,13 +28,42 @@ retorno = eval(firstLine, secondLine);
 function eval(firstLine, secondLine){
     var n = parseInt(firstLine);
     var a = secondLine.split(" ").map( x => parseInt(x) );
-    
+
     console.log(anotherMinimaxProblem(a));
 };
 
 
 function anotherMinimaxProblem(a) {
+    var len = a.length;
+
+    (len%2 == 0) ? a.sort(function(a, b){return a-b}) : a.sort(function(a, b){return b-a});
+
+    console.log("a: " + a + "\tlen: "+ len + "\n\n");
+    var tempA = [];
+    var solutionA = [];
+
+    for(var i = 0 ; i < len/2 ; i++){
+        tempA.push(a[len-i-1]);
+
+        if(len%2 == 0){
+            tempA.push(a[i]);
+        } else{
+            if( (len-i-1) != i ){
+                tempA.push(a[i]);
+            };
+        };
+    };
+
+    console.log("tempA: " + tempA + "\n\n");
+    var max = Number.MIN_SAFE_INTEGER;
+    for(var i = 0 ; i < Math.floor(len -1) ; i++){
+        if( (tempA[i] ^ tempA[i+1]) > max){
+            max = tempA[i] ^ tempA[i+1];
+        };
+        console.log("i: " + i + "\ttempA[" + i + "]: " + tempA[i] + " XOR " + "tempA[" + (i+1) + "]: " + tempA[i+1] + "\tmax: " + max);
+    };
+
+    return max;
+};
 
 
-    return "Hola";
-}
